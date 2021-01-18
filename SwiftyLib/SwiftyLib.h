edc143aca191e4ee3f8ab2edd08d0cbf0b6d0f52
김학철 <hck916@hanpass.com>
2021-01-18 17:07:43 +0900

    first

---
+++ SwiftyLib/SwiftyLib.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyLib.h
+//  SwiftyLib
+//
+//  Created by 김학철 on 2021/01/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLib.
+FOUNDATION_EXPORT double SwiftyLibVersionNumber;
+
+//! Project version string for SwiftyLib.
+FOUNDATION_EXPORT const unsigned char SwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLib/PublicHeader.h>
+
+
